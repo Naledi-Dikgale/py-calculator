d05eb615e6ea41ed6fb29d572bf7cb72741c2168
naledi-dikgale <naledi22khosi@gmail.com>
2025-01-21 17:53:31 +0200

    Refactored

--- main.py
+++ main.py
@@ -1,87 +1,90 @@
-#module imports
+# module imports
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QWidget, QLineEdit, QBoxLayout, QPushButton, QVBoxLayout, QHBoxLayout, QGridLayout
 
-#Main App object and settings
+
+class CalculatorApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        # Main App object and settings
+
+        self.setWindowTitle("Calculator App")
+        self.resize(400, 600)
+
+        # Create all App objects
+        self.text_box = QLineEdit()
+        self.grid = QGridLayout()
+
+        self.buttons = [
+            "7", "8", "9", "/",
+            "4", "5", "6", "*",
+            "1", "2", "3", "-",
+            "0", ".", "=", "+"
+        ]
+
+        self.clear_button = QPushButton("C")
+        self.delete_button = QPushButton("DEL")
+
+        self.create_buttons()
+        self.create_layout()
+
+    def button_click(self):
+        button = self.sender()
+        text = button.text()
+
+        if text == "=":
+            symbol = self.text_box.text()
+            try:
+                result = eval(symbol)
+                self.text_box.setText(str(result))
+
+            except Exception as e:
+                print("Error", e)
+
+        elif text == "C":
+            self.text_box.clear()
+
+        elif text == "DEL":
+            text = self.text_box.text()[:-1]
+            self.text_box.setText(text)
+
+        else:
+            current_value = self.text_box.text()
+            self.text_box.setText(current_value + text)
+
+    def create_buttons(self):
+        row = 0
+        col = 0
+
+        for text in self.buttons:
+            button = QPushButton(text)
+            button.clicked.connect(self.button_click)
+            self.grid.addWidget(button, row, col)
+            col += 1
+
+            if col > 3:
+                col = 0
+                row += 1
+
+    def create_layout(self):
+        master_layout = QVBoxLayout()
+        master_layout.addWidget(self.text_box)
+        master_layout.addLayout(self.grid)
+
+        button_row = QVBoxLayout()
+        button_row.addWidget(self.clear_button)
+        button_row.addWidget(self.delete_button)
+
+        master_layout.addLayout(button_row)
+
+        self.setLayout(master_layout)
+
+        self.clear_button.clicked.connect(self.button_click)
+        self.delete_button.clicked.connect(self.button_click)
+
+
+# Run App
 app = QApplication([])
-main_window = QWidget()
-main_window.setWindowTitle("Calculator App")
-main_window.resize(400, 600)
-
-#Create all App objects
-text_box = QLineEdit()
-grid = QGridLayout()
-
-buttons = [
-    "7", "8", "9", "/",
-    "4", "5", "6", "*",
-    "1", "2", "3", "-",
-    "0", ".", "=", "+"
-]
-
-clear_button = QPushButton("C")
-delete_button = QPushButton("DEL")
-
-def button_click():
-  button = app.sender()
-  text = button.text()
-
-  if text == "=":
-    symbol = text_box.text()
-    try:
-      result = eval(symbol)
-      text_box.setText(str(result))
-
-    except Exception as e:
-      print("Error", e)
-
-  elif text == "C":
-    text_box.clear()
-
-  elif text == "DEL":
-      text = text_box.text()[:-1]
-      text_box.setText(text)
-
-  else:
-    current_value = text_box.text()
-    text_box.setText(current_value + text)
-
-# loop for creating buttons
-
-row = 0
-col = 0
-
-for text in buttons:
-  button = QPushButton(text)
-  button.clicked.connect(button_click)
-  grid.addWidget(button, row, col)
-  col += 1
-
-  if col > 3:
-    col = 0
-    row += 1
-
-#Create Design Layout
-
-master_layout = QVBoxLayout()
-master_layout.addWidget(text_box)
-# master_layout.addLayout(grid)
-master_layout.addLayout(grid)
-
-button_row = QVBoxLayout()
-button_row.addWidget(clear_button)
-button_row.addWidget(delete_button)
-
-master_layout.addLayout(button_row)
-
-# Set the master layout to the main window
-main_window.setLayout(master_layout)
-#Events
-
-clear_button.clicked.connect(button_click)
-delete_button.clicked.connect(button_click)
-
-
-#Run App
+main_window = CalculatorApp()
 main_window.show()
 app.exec_()
